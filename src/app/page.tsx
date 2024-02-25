b17a1c8a45ff45272cfe9c806983809e7bf0b50e
Mirian Montevilla <108299240+Mirian97@users.noreply.github.com>
2024-02-25 18:43:36 -0300

    feat: create initial home page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,11 @@
+import Chat from '@/components/ui/Chat/Chat'
+
 const Home = () => {
-	return <div>Hello World</div>
+	return (
+		<div className='flex min-h-dvh items-center justify-center bg-slate-50'>
+			<Chat />
+		</div>
+	)
 }
 
 export default Home
